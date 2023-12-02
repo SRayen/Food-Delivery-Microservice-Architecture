b80d9b487e616d0258420bd16abebfb2b717a68f
Selmen Rayen <selmanerayene95@gmail.com>
2023-12-02 02:21:37 +0100

    Add User Registration & Activation with OTP Verification

--- clients/user-ui/src/screens/AuthScreen.tsx
+++ clients/user-ui/src/screens/AuthScreen.tsx
@@ -1,14 +1,28 @@
 import { useState } from "react";
 import Login from "../shared/Auth/Login";
 import Signup from "../shared/Auth/Signup";
+import Verification from "../shared/Auth/Verification";
 
-const AuthScreen = () => {
+const AuthScreen = ({ setOpen }: { setOpen: (e: boolean) => void }) => {
   const [activeState, setActiveState] = useState("Login");
+  //To close Modal after outside click:
+  const handleClose = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+    if (e.target instanceof HTMLDivElement && e.target.id === "screen") {
+      setOpen(false);
+    }
+  };
   return (
-    <div className="w-full fixed top-11 left-0 h-screen z-50 flex items-center justify-center bg-[#00000027]">
+    <div
+      id="screen"
+      className="w-full fixed top-11 left-0 h-screen z-50 flex items-center justify-center bg-[#00000027]"
+      onClick={handleClose}
+    >
       <div className="w-[450px] bg-slate-900 rounded shadow-sm p-1">
         {activeState === "Login" && <Login setActiveState={setActiveState} />}
         {activeState === "Signup" && <Signup setActiveState={setActiveState} />}
+        {activeState === "Verification" && (
+          <Verification setActiveState={setActiveState} />
+        )}
       </div>
     </div>
   );
