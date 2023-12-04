cf9141d77c1d40be6d3d4b7b903f9af842035972
Selmen Rayen <selmanerayene95@gmail.com>
2023-12-04 19:44:45 +0100

    Get LoggedInUser & Create Logout

---
+++ clients/user-ui/src/graphql/actions/login.action.ts
@@ -0,0 +1,21 @@
+"use client";
+import { gql, DocumentNode } from "@apollo/client";
+
+export const LOGIN_USER: DocumentNode = gql`
+  mutation LoginUser($email: String!, $password: String!) {
+    Login(email: $email, password: $password) {
+      user {
+        name
+        email
+        phone_number
+        address
+        phone_number
+      }
+      accessToken
+      refreshToken
+      error {
+        message
+      }
+    }
+  }
+`;
--- clients/user-ui/src/graphql/gql.setup.ts
+++ clients/user-ui/src/graphql/gql.setup.ts
@@ -1,8 +1,35 @@
 //GraphQL Setup Code:
 
-import { ApolloClient, InMemoryCache } from "@apollo/client";
+import {
+  ApolloClient,
+  ApolloLink,
+  InMemoryCache,
+  createHttpLink,
+} from "@apollo/client";
 
-export const graphqlClient=new ApolloClient({
-    uri:process.env.NEXT_PUBLIC_SERVER_URI,
-    cache:new InMemoryCache()
-})
+import Cookies from "js-cookie";
+
+//Simple Setup:
+// export const graphqlClient=new ApolloClient({
+//     uri:process.env.NEXT_PUBLIC_SERVER_URI,
+//     cache:new InMemoryCache()
+// })
+
+const httpLink = createHttpLink({
+  uri: process.env.NEXT_PUBLIC_SERVER_URI,
+});
+
+const authMiddleware = new ApolloLink((oparetion, forward) => {
+  oparetion.setContext({
+    headers: {
+      accesstoken: Cookies.get("access_token"),
+      refreshtoken: Cookies.get("refresh_token"),
+    },
+  });
+  return forward(oparetion);
+});
+
+export const graphqlClient = new ApolloClient({
+  link: authMiddleware.concat(httpLink),
+  cache: new InMemoryCache(),
+});
